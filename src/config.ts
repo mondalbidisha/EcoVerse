c550faa90fa39c44f0ef98d7606471709e0a8b36
Bidisha Mondal <mondal.bidisha@gmail.com>
2024-07-11 16:10:56 +0530

    added backend deployed server url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const GITHUB_CONTRIBUTOR_URL = '';
-export const BACKEND_URL = "http://localhost:8787";
+// export const BACKEND_URL = "http://localhost:8787";
+export const BACKEND_URL = "https://ecoverse-backend.mondal-bidisha.workers.dev";
 export const FF_ENABLE_AI = true; 
 export const FF_IMAGE_UPLOADS = false; 
