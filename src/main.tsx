6d1e93338f9ac282c6daf3f58025c4f8f33d57ba
Bidisha Mondal <mondal.bidisha@gmail.com>
2024-07-16 13:43:56 +0530

    service worker registeration code changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { registerSW } from 'virtual:pwa-register';
 
 if ('serviceWorker' in navigator) {
-  registerSW({ immediate: true });
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/firebase-messaging-sw.js').then(registration => {
       console.log('Service Worker registered with scope: ', registration.scope);
